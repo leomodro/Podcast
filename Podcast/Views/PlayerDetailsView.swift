d567fda3bf51170023fe6556fefcb9e53e925b6c
Leonardo Modro <leonardomodro@gmail.com>
2018-03-13 16:00:21 -0300

    Playing and pausing the podcast

--- Podcast/Views/PlayerDetailsView.swift
+++ Podcast/Views/PlayerDetailsView.swift
@@ -7,22 +7,29 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
     var episode: Episode! {
         didSet {
             episodeTitleLabel.text = episode.title
             authorLabel.text = episode.author
             guard let url = URL(string: episode.imageUrl?.toSecureHTTPS() ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
+            
+            playEpisode()
         }
     }
     
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
-    
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBOutlet weak var episodeTitleLabel: UILabel! {
         didSet {
@@ -30,5 +37,28 @@ class PlayerDetailsView: UIView {
         }
     }
     @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    
+    //MARK: - Play / Pause Episode
+    private func playEpisode() {
+        guard let url = URL(string: episode.streamUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+    @objc func handlePlayPause() {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+    }
 }
